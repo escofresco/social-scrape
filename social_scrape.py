101923e0c7d9fece674b1c477a28347a34a01e8b
Jonasz Karwacki <jonaszakr@gmail.com>
2018-06-18 16:12:52 -0700

    Add breadth first search

--- social_scrape.py
+++ social_scrape.py
@@ -1,11 +1,7 @@
-import bs4
+import bs4 import BeautifulSoup
 from extract_social_media import find_links_tree
 from html_to_etree import parse_html_bytes
 import requests
-# res = requests.get('http://vsolvit.com/')
-# tree = parse_html_bytes(res.content, res.headers.get('content-type'))
-#
-# print(set(find_links_tree(tree)))
 
 def find_social_links(url) -> set:
     """
@@ -16,4 +12,21 @@ def find_social_links(url) -> set:
     tree = parse_html_bytes(response.content, response.headers.get('content-type'))
     return set(find_links_tree(tree))
 
-print(find_social_links("http://vsolvit.com/"))
+#print(find_social_links("http://vsolvit.com/"))
+def page_links(html_cnt) -> set:
+    soup = BeautifulSoup(html_cnt, 'lxml')
+    links = set()
+    for link in set(soup.find_all('a')):
+        if link['href'].startswith('/') or link['href'].startswith('\'):
+            links.add(link['href'])
+    return links
+
+def bfs(url) -> set :
+    social_links = find_social_links(url)
+    if (len(social_links) > 0):
+        return social_links
+    else:
+        response = requests.get(url)
+        links
+
+if __name__ == "__main__":
