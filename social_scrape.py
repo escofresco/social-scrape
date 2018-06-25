eadc2bde82a0fa45510935c7c3140c7a3ee9915f
Jonasz Karwacki <jonaszakr@gmail.com>
2018-06-25 14:17:26 -0700

    Add command line support

--- social_scrape.py
+++ social_scrape.py
@@ -1,8 +1,11 @@
-import bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from extract_social_media import find_links_tree
+import getopt
 from html_to_etree import parse_html_bytes
 import queue
 import requests
+import sys
+
 
 def find_social_links(url) -> set:
     """
@@ -18,7 +21,7 @@ def page_links(html_cnt) -> set:
     soup = BeautifulSoup(html_cnt, 'lxml')
     links = set()
     for link in set(soup.find_all('a')):
-        if link['href'].startswith('/') or link['href'].startswith('\'):
+        if link['href'].startswith('/') or link['href'].startswith('\\'):
             links.add(link['href'])
     return links
 
@@ -34,5 +37,20 @@ def bfs(url) -> set:
             response = requests.get(url)
             links = page_links(response)
 
+def main(argv):
+    url = ''
+    try:
+        opts, args = getopt.getopt(argv, 'hu:', ['help', 'url='])
+    except getopt.GetoptError:
+        print('social_scrape.py -u <url>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print('social_scrape.py -u <url>')
+            sys.exit()
+        elif opt in ('-u', '--url'):
+            url = arg
+    print('Url is '+url)
 
 if __name__ == "__main__":
+    main(sys.argv[1:])
